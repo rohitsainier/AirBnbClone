feebf7e47e3d97816019c2a3e63899a644e9cf02
rohitsainier <rohitsainier@gmail.com>
2021-01-12 22:03:25 +0530

    Code Updated for home screen design

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
+
+import HomeScreen from './scr/screens/Home';
+
+const App = () => {
+  return(
+    <>
+    <StatusBar barStyle="dark-content"/>
+    <SafeAreaView>
+      <HomeScreen />
+    </SafeAreaView>
+    </>
+  );
+}
+
+
+
+export default App;
