4216dbf3b63591ae5ca1034a4d65f735ff6433d6
rohitsainier <rohitsainier@gmail.com>
2021-01-13 21:36:41 +0530

    Post component design complete

--- scr/components/Post/index.js
+++ scr/components/Post/index.js
@@ -14,6 +14,14 @@ const Post = (props) =>{
             source={{uri:'https://storage.googleapis.com/gweb-uniblog-publish-prod/images/photos_drive_vector-01.max-1000x1000.png'}}
         >
         </Image>
+        <Text style={styles.bedrooms}>1 bed 1 bedroom</Text>
+        <Text style={styles.description} numberOfLines={2}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</Text>
+        <Text style={styles.prices}>
+            <Text style={styles.oldPrice}>$43</Text>
+            <Text style={styles.newPrice}>  $36</Text>
+            /night
+        </Text>
+        <Text style={styles.totalPrice}>$230 total</Text>
         </View> 
     );
 };
--- scr/components/Post/styles.js
+++ scr/components/Post/styles.js
@@ -9,7 +9,30 @@ const styles = StyleSheet.create({
     },
     container:{
         margin:20,
-    }
+    },
+    bedrooms:{
+        marginVertical:10,
+        color:"#5b5b5b",
+    },
+    description:{
+       fontSize:20,
+       lineHeight:26,
+    },
+    prices:{
+        fontSize:18,
+        marginVertical:10,
+    },
+    oldPrice:{
+        color:"#5b5b5b",
+        textDecorationLine:'line-through',
+    },
+    newPrice:{
+        fontWeight:'bold',
+    },
+    totalPrice:{
+        color:'#5b5b5b',
+        textDecorationLine:'underline',
+    },
 });
 
 export default styles;
